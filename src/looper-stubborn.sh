ae3b8472b21f78a7d47729e3ffb9acac2e54de2c
Michael Lee <lee@iit.edu>
2025-03-03 17:13:03 -0600

    Adding counter output and renaming file

--- src/looper-stubborn.sh
+++ src/looper-stubborn.sh
@@ -7,7 +7,9 @@ handler() {
 trap handler SIGINT
 # trap handler SIGTERM # uncomment to ignore default kill
 
+count=1
 while true; do
-  echo "Looping..."
+  echo "Loop $count in $$"
+  ((count++))
   sleep 1
 done
